315b7a0759db80ef8baaf0dc99fdd1c2b5b32c3a
Robert Stevens <robert@stevens.tk>
2014-10-10 16:17:04 +0200

    motor test

--- rs_bot_base.py
+++ rs_bot_base.py
@@ -17,7 +17,7 @@ def callback(data):
     rospy.loginfo(rospy.get_name() + " angular.x %d angular.y %d angular.z %d", data.angular.x, data.angular.y,
                   data.angular.z)
 
-    if (data.linear.x > 255)
+    if (data.linear.x > 255):
 	data.linear.x = 255
     motor.set_motor_speed(0, int(data.linear.x))
     motor.set_motor_speed(1, int(data.linear.x))
