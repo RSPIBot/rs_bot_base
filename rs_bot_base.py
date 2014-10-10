a575ce61aceb931c364d7af7aaff819fd9cf2dbb
Robert Stevens <robert@stevens.tk>
2014-10-10 15:02:47 +0200

    motor test

--- rs_bot_base.py
+++ rs_bot_base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-#from MotorClass import MotorClass
+from MotorClass import MotorClass
 import time
 import roslib;
 
@@ -9,12 +9,15 @@ import rospy
 
 from geometry_msgs.msg import Twist
 
+MotorClass motor
 
 def callback(data):
     rospy.loginfo(rospy.get_name() + " linear.x %d linear.y %d linear.z %d", data.linear.x, data.linear.y,
                   data.linear.z)
     rospy.loginfo(rospy.get_name() + " angular.x %d angular.y %d angular.z %d", data.angular.x, data.angular.y,
                   data.angular.z)
+    motor.set_motor_speed(1, 4000)
+    motor.change_motor_speed()
 
 def listener():
     rospy.Subscriber("cmd_vel", Twist, callback)
