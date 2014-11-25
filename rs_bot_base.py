ee5f395b538b25e30542f5af53fa2f5f2e01049a
Robert Stevens <robert@stevens.tk>
2014-11-25 16:37:53 +0100

    updated the driving

--- rs_bot_base.py
+++ rs_bot_base.py
@@ -14,27 +14,39 @@ motor = MotorClass()
 def callback(data):
     rospy.loginfo(rospy.get_name() + " linear.x %d and angular.z %d", data.linear.x, data.angular.z)
 
-    if (data.linear.x > 254):
-        data.linear.x = 254
-    if (data.linear.x < -254):
-        data.linear.x = (-254)
+    #limit the values between -1 and 1 to have simular results as the turtlebot
+    if (data.linear.x > 1):
+        data.linear.x = 1
+    if (data.linear.x < -1):
+        data.linear.x = (-1)
 
-    if (data.angular.z > 254):
-        data.angular.z = 254
-    if (data.angular.z < -254):
-        data.angular.z = (-254)
-		
-    right = data.linear.x
-    left = data.linear.x
-    if (data.angular.z < 0):
-        right = (data.angular.z*(-1))
-        left = (data.angular.z)
-        rospy.loginfo( "turn right left: %d and right: %d", left, right)
-    if (data.angular.z > 0):
-        left = data.angular.z
-        right = (data.angular.z*(-1))
-        rospy.loginfo( "turn left left: %d and right: %d", left, right)
-	
+    if (data.angular.z > 1):
+        data.angular.z = 1
+    if (data.angular.z < -1):
+        data.angular.z = (-1)
+
+	#scale the values back
+	data.linear.x *= 255
+    data.angular.z *= 255
+
+    speed = data.linear.x
+    turnAngle = data.angular.z
+    wheelDistence = 11
+    correction = 1
+    #right = data.linear.x
+    #left = data.linear.x
+    #if (data.angular.z < 0):
+    #    right = (data.angular.z*(-1))
+    #    left = (data.angular.z)
+    #    rospy.loginfo( "turn right left: %d and right: %d", left, right)
+    #if (data.angular.z > 0):
+    #    left = data.angular.z
+    #    right = (data.angular.z*(-1))
+    #    rospy.loginfo( "turn left left: %d and right: %d", left, right)
+
+    right = speed + (2/wheelDistence)*(turnAngle) * correction
+    left = speed + (2/wheelDistence)*(turnAngle) * correction
+
     motor.set_motor_speed(0, int(left))
     motor.set_motor_speed(1, int(right))
     motor.set_motor_speed(2, int(left))
